5bf44e3b8ae3b93edf009b3d9afb168d068edc34
eneseray <enes.eraay@gmail.com>
2022-06-23 21:31:32 +0300

    file reset.

--- mylibrary/src/main/java/com/example/mylibrary/Exceptions.kt
+++
@@ -1,5 +0,0 @@
-package com.example.mylibrary
-
-class NullObjectException(message: String) : Exception(message)
-class ConflictingParametersException(message: String) : Exception(message)
-class EmptyFieldException(message: String) : Exception(message)
